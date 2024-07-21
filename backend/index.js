344b016747b3c26ad87d41dfa39f7143d1929f22
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-07-21 10:16:01 +0500

    deploy

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cityRoutes from "./routes/cities.routes.js";
-//c
 
 dotenv.config();
 const app = express();
