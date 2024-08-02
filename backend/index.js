d493821abcb7e5e07975dae54fcb9749399e4e54
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-08-02 23:34:26 +0500

    deploy

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://sajda-cli.vercel.app/api"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
